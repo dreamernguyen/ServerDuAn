69f9025a4d88285fc3974fef65bf0146a0d7ef63
dreamernguyen <55968031+dreamernguyen@users.noreply.github.com>
2021-03-18 15:03:33 +0700

    Update

--- controllers/nguoiDungController.js
+++ controllers/nguoiDungController.js
@@ -5,17 +5,19 @@ import QRCode from 'qrcode'
 export async function dangKy(req, res) {
   const nguoiDungMoi = new NguoiDung(req.body);
   try {
-    if (await NguoiDung.findOne({ email: req.body.email })) {
-      res.send("Email này đã được đăng ký !");
+    if (await NguoiDung.findOne({ sdt: req.body.sdt })) {
+      res.send({
+        thongBao: 'Số điện thoại này đã được đăng ký'
+      });
     } else {
       await nguoiDungMoi.save();
       res.send({
-        thongBao: `Đăng ký thành công với email ${req.body.email}`,
+        thongBao: `Đăng ký thành công với ${req.body.hoTen}`,
       });
       console.log(nguoiDungMoi);
     }
   } catch (err) {
-    res.status(400).send(err.message);
+    console.log(err);
   }
 }
 
